1e5929210e45c50a5839325ca08fc59d4b9d3f09
Cora Reyes Calens <urbanusjam@gmail.com>
2017-09-24 18:29:21 -0300

    Second interation

--- src/main/java/org/urbanusjam/rpgcombat/Character.java
+++ src/main/java/org/urbanusjam/rpgcombat/Character.java
@@ -1,5 +1,6 @@
 package org.urbanusjam.rpgcombat;
 
+import static java.lang.String.format;
 
 public class Character {
 
@@ -38,25 +39,66 @@ public class Character {
         this.alive = alive;
     }
 
-    public void dealDamage(Character opponent, int damageQty) {
-        if(damageQty > opponent.getHealth()) {
-            opponent.setHealth(0);
-            opponent.setAlive(false);
+    public void applyDamage(Character target, int damageQty) {
+        int realDamage = damageQty;
+        if(target == this) {
+            throw new UnsupportedOperationException("A character cannot apply damage to himself");
+        }
+        if((target.getLevel() - this.level) >= 5) {
+            realDamage = damageQty / 2;
+        }
+        else if((target.getLevel() - this.level) <= -5) {
+            realDamage = damageQty + (damageQty / 2);
+        }
+        if(damageQty > target.getHealth()) {
+            target.setHealth(0);
+            target.setAlive(false);
         }
         else {
-            opponent.setHealth(opponent.getHealth() - damageQty);
+            target.setHealth(target.getHealth() - realDamage);
         }
     }
 
-    public void heal(Character opponent, int healQty) {
-        if(!opponent.isAlive()) {
-            throw new UnsupportedOperationException("A dead character cannot be healed");
-        }
-        if(opponent.getHealth() + healQty > MAX_HEALTH) {
-            opponent.setHealth(MAX_HEALTH);
+    public void heal(Character target, int healQty) {
+        if(target == this) {
+            if(!target.isAlive()) {
+                throw new UnsupportedOperationException("A dead character cannot be healed");
+            }
+            if(target.getHealth() + healQty > MAX_HEALTH) {
+                target.setHealth(MAX_HEALTH);
+            }
+            else {
+                target.setHealth(target.getHealth() + healQty);
+            }
         }
         else {
-            opponent.setHealth(opponent.getHealth() + healQty);
+            throw new UnsupportedOperationException("A character cannot heal enemies");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Character))
+            return false;
+        Character character = (Character) o;
+        return character.health == health &&
+                character.level == level &&
+                character.alive == alive;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + health;
+        result = 31 * result + level;
+        result = 31 * result + (alive ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return format("Character [health=%s, level=%s, %s]", health, level, alive ? "ALIVE" : "DEAD");
     }
 }
--- src/test/java/org/urbanusjam/rpgcombat/CharacterTest.java
+++ src/test/java/org/urbanusjam/rpgcombat/CharacterTest.java
@@ -9,78 +9,112 @@ import static org.hamcrest.Matchers.is;
 
 public class CharacterTest {
 
-    private Character hero;
-    private Character enemy;
+    private Character attacker;
+    private Character target;
 
     @Before
     public void setUp() {
-        hero = new Character();
-        enemy = new Character();
+        attacker = new Character();
+        target = new Character();
     }
 
     //1. All Characters, when created, have:
-    //        - Health, starting at 1000
-    //        - Level, starting at 1
-    //        - May be Alive or Dead, starting Alive (Alive may be a true/false)
+    //   - Health, starting at 1000
+    //   - Level, starting at 1
+    //   - May be Alive or Dead, starting Alive (Alive may be a true/false)
+    //2. Characters can Deal Damage to Characters.
+    //   - Damage is subtracted from Health
+    //   - When damage received exceeds current Health, Health becomes 0 and the character dies
+    //3. A Character can Heal a Character.
+    //   - Dead characters cannot be healed
+    //   - Healing cannot raise health above 1000
+
     @Test
     public void whenCharacterIsCreatedThenHealthStartsAt1000(){
-        assertThat(hero.getHealth(), is(1000));
+        assertThat(attacker.getHealth(), is(1000));
     }
 
     @Test
     public void whenCharacterIsCreatedThenLevelStartsAt1(){
-        assertThat(hero.getLevel(), is(1));
+        assertThat(attacker.getLevel(), is(1));
     }
 
     @Test
     public void whenCharacterIsCreatedThenIsAlive(){
-        assertThat(hero.isAlive(), is(true));
-    }
-
-    //2. Characters can Deal Damage to Characters.
-    //   - Damage is subtracted from Health
-    //   - When damage received exceeds current Health, Health becomes 0 and the character dies
-    @Test
-    public void whenCharacterIsDamagedAndDamageIsLowerThanCurrentHealthThenDamageIsSubtractedFromHealth() {
-        hero.dealDamage(enemy, 100);
-        assertThat(enemy.getHealth(), is(900));
+        assertThat(attacker.isAlive(), is(true));
     }
 
     @Test
-    public void whenCharacterIsDamagedAndDamageExceedsCurrentHealthThenHealthIs0AndCharacterDies() {
-        hero.dealDamage(enemy, 2000);
-        assertThat(enemy.getHealth(), is(0));
-        assertThat(enemy.isAlive(), is(false));
-    }
-
-    //3. A Character can Heal a Character.
-    //   - Dead characters cannot be healed
-    //   - Healing cannot raise health above 1000
-    @Test
-    public void whenCharacterIsHealedAndAliveThenCurrentHealthIsIncreasesByTheAmountOfHealReceived() {
-        hero.dealDamage(enemy, 100);
-        assertThat(enemy.getHealth(), is(900));
-        hero.heal(enemy, 100);
-        assertThat(enemy.getHealth(), is(1000));
+    public void whenCharacterDealsDamageAndDamageIsLowerThanCurrentHealthThenDamageIsSubtractedFromHealth() {
+        attacker.applyDamage(target, 100);
+        assertThat(target.getHealth(), is(900));
     }
 
     @Test
-    public void whenCharacterIsHealedAndAliveAndNewHealthIsAbove1000ThenHealthIsSetTo1000() {
-        hero.dealDamage(enemy, 100);
-        hero.heal(enemy, 1100);
-        assertThat(enemy.getHealth(), is(1000));
+    public void whenCharacterDealsDamageAndDamageExceedsCurrentHealthThenHealthIs0AndCharacterDies() {
+        target.applyDamage(attacker, 2000);
+        assertThat(attacker.getHealth(), is(0));
+        assertThat(attacker.isAlive(), is(false));
+    }
+
+    @Test
+    public void whenCharacterHealsAndIsAliveThenCurrentHealthIncreasesByTheAmountOfHealReceived() {
+        target.applyDamage(attacker, 100);
+        assertThat(attacker.getHealth(), is(900));
+
+        attacker.heal(attacker, 100);
+        assertThat(attacker.getHealth(), is(1000));
+    }
+
+    @Test
+    public void whenCharacterHealsAndIsAliveAndNewHealthIsAbove1000ThenHealthIsSetTo1000() {
+        target.applyDamage(attacker, 100);
+        assertThat(attacker.getHealth(), is(900));
+
+        attacker.heal(attacker, 900);
+        assertThat(attacker.getHealth(), is(1000));
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void whenCharacterIsHealedAndDeadThenCharacterCannotBeHealed() {
-        hero.dealDamage(enemy, 2000);
-        hero.heal(enemy, 100);
+        target.applyDamage(attacker, 2000);
+        attacker.heal(attacker, 100);
+    }
+
+    //1. The Character can deal damage to his enemies, but not to himself.
+    //2. The Character can heal himself, but not his enemies.
+    //3. The level now has an effect on the damage applied:
+    //   - If the target is 5 or more levels above the attacker, Damage is reduced by 50%
+    //   - If the target is 5 or more levels below the attacker, Damage is increased by 50%
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenCharacterDamagesHimselfThenDamageCannotBeDone() {
+        attacker.applyDamage(attacker, 100);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenCharacterHealsAnEnemyThenHealingCannotBeDone() {
+        attacker.heal(target, 100);
+    }
+
+    @Test
+    public void whenTargetIsFiveOrMoreLevelsAboveAttackerThenDamageIsReducedBy50Percent () {
+        target.setLevel(6);
+        attacker.applyDamage(target, 100);
+        assertThat(target.getHealth(), is(950));
+    }
+
+    @Test
+    public void whenTargetIsFiveOrMoreLevelsBelowAttackerThenDamageIsIncreasedBy50Percent () {
+        attacker.setLevel(6);
+        attacker.applyDamage(target, 100);
+        assertThat(target.getHealth(), is(850));
     }
 
     @After
     public void tearDown() {
-        hero = null;
-        enemy = null;
+        attacker = null;
+        target = null;
     }
 
 }
